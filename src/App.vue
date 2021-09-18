c3fb96c27b36cfcdeabcfdc528f242ad3bdc3bdd
bill <phongbinh2511@gmail.com>
2021-09-18 23:23:00 +0800

    [master] Added views for Panama, Hawaii, and Jamaica.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/brazil">Brazil</router-link>
+      <router-link to='/brazil'>Brazil</router-link>
+      <router-link to='/panama'>Panama</router-link>
+      <router-link to='/hawaii'>Hawaii</router-link>
+      <router-link to='/jamaica'>Jamaica</router-link>
     </div>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Brazil from '../views/Brazil.vue'
+import Panama from '../views/Panama.vue'
+import Hawaii from '../views/Hawaii.vue'
+import Jamaica from '../views/Jamaica.vue'
 
 Vue.use(VueRouter);
 
@@ -24,6 +27,21 @@ const routes = [
     'path': '/brazil',
     'name': 'brazil',
     'component': Brazil
+  },
+  {
+    'path': '/panama',
+    'name': 'panama',
+    'component': Panama
+  },
+  {
+    'path': '/hawaii',
+    'name': 'hawaii',
+    'component': Hawaii
+  },
+  {
+    'path': '/jamaica',
+    'name': 'jamaica',
+    'component': Jamaica
   }
 ];
 
