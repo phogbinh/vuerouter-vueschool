d00f74e049bc7714c5eea1a85de2114644a9a267
bill <phongbinh2511@gmail.com>
2021-09-18 23:15:52 +0800

    [master] Added brazil page component.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Brazil from '../views/Brazil.vue'
 
 Vue.use(VueRouter);
 
@@ -19,6 +20,11 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    'path': '/brazil',
+    'name': 'brazil',
+    'component': Brazil
+  }
 ];
 
 const router = new VueRouter({
