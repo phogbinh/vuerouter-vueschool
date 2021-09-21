477c1e8936b6602c856b356a8db1eb8840bf5aa9
bill <phongbinh2511@gmail.com>
2021-09-21 22:35:37 +0800

    [master] Moved nav styling to TheNavigation component.

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -12,5 +12,14 @@
 #nav {
   display: flex;
   justify-content: center;
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 0 10px;
+}
+#nav a.vue-school-active-class {
+  color: purple;
 }
 </style>
