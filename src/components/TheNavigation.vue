0aa935016b8a6ec51ee371c88d0efe2e6e873ac3
bill-msi-laptop <bill-msi-laptop>
2021-09-25 23:49:18 +0800

    [master] Replaced static router links with dynamic ones.

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,25 +1,57 @@
 <template>
   <nav id='nav'>
-    <router-link to='/'>Home</router-link>
-    <router-link to='/brazil'>Brazil</router-link>
-    <router-link to='/panama'>Panama</router-link>
-    <router-link to='/hawaii'>Hawaii</router-link>
-    <router-link to='/jamaica'>Jamaica</router-link>
+    <ul class='nav-links'>
+      <p class='logo'>The Vue School Travel App</p>
+      <li class='links'>
+        <router-link to='/'>Home</router-link>
+      </li>
+      <li v-for='destination in destinations'
+      :key='destination["name"]'
+      class='links'>
+        <router-link :to='{"name": "DestinationDetails", "params": {"id": destination["id"]}}'>{{ destination['name'] }}</router-link>
+      </li>
+    </ul>
   </nav>
 </template>
+
+<script>
+import store from '@/store.js'
+
+export default {
+  'data': function() {
+    return {
+      'destinationId': this['$route']['params']['id'],
+      'destinations': store['destinations']
+    } 
+  }
+}
+</script>
 
 <style scoped>
 #nav {
   display: flex;
-  justify-content: center;
-  padding: 30px;
 }
 #nav a {
-  font-weight: bold;
   color: #2c3e50;
-  padding: 0 10px;
+  text-decoration: none;
+  font-weight: bold;
 }
 #nav a.vue-school-active-class {
   color: purple;
+}
+.nav-links {
+  display: flex;
+}
+.links {
+  padding-right: 20px;
+  list-style: none;
+}
+.links:hover {
+  text-decoration: underline;
+}
+.logo {
+  font-size: 20px;
+  color: purple;
+  font-weight: bold;
 }
 </style>
