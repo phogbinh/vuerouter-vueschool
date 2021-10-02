e2f3550dccabcdd1ef64bac4a1f6cebb2b064999
kyo <phongbinh2511@gmail.com>
2021-10-03 00:29:25 +0800

    [master] Added experience UI.

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,11 +1,27 @@
 <template>
-  <section class='destination'>
-    <h1>{{ destination['name'] }}</h1>
-    <div class='destination-details'>
-      <img :src='require(`@/assets/${destination["image"]}`)' :alt='destination["name"]'>
-      <p>{{ destination['description'] }}</p>
-    </div>
-  </section>
+  <div>
+    <section class='destination'>
+      <h1>{{ destination['name'] }}</h1>
+      <div class='destination-details'>
+        <img :src='require(`@/assets/${destination["image"]}`)' :alt='destination["name"]'>
+        <p>{{ destination['description'] }}</p>
+      </div>
+    </section>
+    <section class='experiences'>
+      <h2>Top experiences in {{ destination['name'] }}</h2>
+      <div class='cards'>
+        <div v-for='experience in experiences'
+        :key='experience["slug"]'
+        class='card'>
+          <img :src='require(`@/assets/${experience["image"]}`)'
+          :alt='experience["name"]'>
+          <span class='card-text'>
+            {{ experience['name'] }}
+          </span>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -46,5 +62,26 @@ p {
   margin: 0 40px;
   font-size: 20px;
   text-align: left;
+}
+.cards {
+  display: flex;
+  justify-content: space-between;
+}
+.cards img {
+  max-height: 200px;
+}
+.card {
+  padding: 0 20px;
+  position: relative;
+}
+.card__text {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 25px;
+  font-weight: bold;
+  text-decoration: none;
 }
 </style>
