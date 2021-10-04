bbee8943ebaf81f116dfbb7163f2295cf41e7774
bill-msi-laptop <bill-msi-laptop>
2021-10-04 23:35:27 +0800

    [master] Added go back button.

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <GoBack />
     <section class='destination'>
       <h1>{{ destination['name'] }}</h1>
       <div class='destination-details'>
@@ -34,8 +35,12 @@
 
 <script>
 import store from '@/store.js'
+import GoBack from '@/components/GoBack.vue'
 
 export default {
+  'components': {
+    GoBack
+  },
   'data': function() {
     return {}
   },
