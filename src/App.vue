5a62621d840ab7ad80941c4f486a819d067c0692
bill-msi-laptop <bill-msi-laptop>
2021-10-06 11:17:55 +0800

    [master] Replaced moveUp with fade -- an even fucked up glitcher animation.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <TheNavigation />
-    <transition name='moveUp'>
+    <transition name='fade'>
       <router-view :key='$route["path"]' />
     </transition>
   </div>
@@ -25,29 +25,12 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-.moveUp-enter-active {
-  animation: fadeIn 1s ease-in;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
 }
-@keyframes fadeIn {
-  0% {
-    opacity: 0;
-  }
-  50% {
-    opacity: 0.5;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-.moveUp-leave-active {
-  animation: moveUp 0.3s ease-in;
-}
-@keyframes moveUp {
-  0% {
-    transform: translateY(0);
-  }
-  100% {
-    transform: translateY(-400px);
-  }
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
