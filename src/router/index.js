4c19e0b1af321afc996e914466630c39e9b8f22f
bill-msi-laptop <bill-msi-laptop>
2021-10-06 13:22:28 +0800

    [master] Added navigation guard for experienceDetails.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from '@/store.js'
 
 Vue.use(VueRouter);
 
@@ -23,7 +24,18 @@ const routes = [
         'props': true,
         'component': () => import(/* webpackChunkName: 'ExperienceDetails' */ '../views/ExperienceDetails.vue')
       }
-    ]
+    ],
+    'beforeEnter': function(to, from, next) {
+      const exists = store['destinations'].find(
+        destination => destination['slug'] === to['params']['slug']
+      )
+      if (exists) {
+        next()
+      }
+      else {
+        next({'name': 'notFound'})
+      }
+    }
   },
   {
     'path': '*',
