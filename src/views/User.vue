7164cfb86865ca3ff72e8f432ea1959600177e33
bill-msi-laptop <bill-msi-laptop>
2021-10-19 21:47:18 +0800

    [master] Added log out function.

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>user</h1>
+  <div>
+    <h1>user</h1>
+    <button @click='logOut' class='btn'>Log out</button>
+  </div>
 </template>
+<script>
+import store from '@/store.js'
+
+export default {
+  'methods': {
+    'logOut': function() {
+      store['user'] = null
+      this['$router'].push('/')
+    }
+  }
+}
+</script>
