aa7d97bacdd95002696e3bfa3c2a20252a2ee95e
bill-msi-laptop <bill-msi-laptop>
2021-10-21 22:49:53 +0800

    [master] Added redirect to home on login.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -23,7 +23,8 @@ export default {
   'methods': {
     'login': function() {
       store['user'] = this['username']
-      this['$router'].push('/user')
+      const redirectPath = this['$route']['query']['redirect'] || '/'
+      this['$router'].push(redirectPath)
     }
   }
 }
